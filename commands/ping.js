dca6482f59c04504e8223b13606ed39e046b0f1a
fahim-md <msmaimrfahim@gmail.com>
2026-06-19 15:44:05 +0600

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -35,7 +35,7 @@ async function pingCommand(sock, chatId, message) {
         const response =
             `*${t.ping_pong}*\n` +
             `${t.ping_response} *${latency}* ${t.ping_seconds}\n\n` +
-            `*💻 ${settings.botName || "Queen Riam"}* ${t.ping_server_info}\n` +
+            `*💻 ${settings.botName || "Fahim bbz"}* ${t.ping_server_info}\n` +
             `${t.ping_ram} *${ramUsage}*\n` +
             `${t.ping_cores} *${cpus.length}*\n` +
             `${t.ping_speed} *${(cpu.speed / cpus.length).toFixed(2)} MHz*`;
@@ -43,7 +43,7 @@ async function pingCommand(sock, chatId, message) {
         if (isButtonModeOn()) {
             await sendButtonMessage(sock, chatId, {
                 text: response,
-                footer: `${settings.botName || "Queen Riam"} 👑`,
+                footer: `${settings.botName || "Fahim bbz"} 👑`,
                 buttons: [
                     { id: '.alive', text: t.ping_alive_btn },
                 ],
@@ -54,8 +54,8 @@ async function pingCommand(sock, chatId, message) {
                     forwardingScore: 999,
                     isForwarded: true,
                     forwardedNewsletterMessageInfo: {
-                        newsletterJid: "120363404284793169@newsletter",
-                        newsletterName: settings.botName || "Queen Riam",
+                        newsletterJid: "",
+                        newsletterName: settings.botName || "",
                         serverMessageId: -1
                     }
                 }
